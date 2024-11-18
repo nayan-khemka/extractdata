9248414df79722e5e9f763d1d651b4c3d80de73f
Nayan Khemka <72384862+nayan-khemka@users.noreply.github.com>
2024-11-18 16:07:43 +0530

    Update program.py

--- program.py
+++ program.py
@@ -11,15 +11,18 @@ def fetch_and_extract_tables(url):
     data_frames = []
     for table in tables:
         df = pd.read_html(str(table))[0]
-        if df.applymap(lambda x: isinstance(x, (int, float))).any().any():
-            data_frames.append(df)
+        data_frames.append(df)
 
     return data_frames
 
 def save_to_excel(data_frames, filename):
     with pd.ExcelWriter(filename) as writer:
-        for i, df in enumerate(data_frames):
-            df.to_excel(writer, sheet_name=f'Table_{i+1}')
+        if data_frames:  # Check if there's at least one DataFrame
+            for i, df in enumerate(data_frames):
+                df.to_excel(writer, sheet_name=f'Table_{i+1}')
+        else:
+            # Create at least one empty sheet if there are no data frames
+            pd.DataFrame().to_excel(writer, sheet_name='Empty')
 
 def main():
     url = 'https://teams.microsoft.com/l/message/19:02af48fa-f7ab-4f7a-9063-146672972066_6e9bbcc8-378d-464a-be47-9fb6b06c76f8@unq.gbl.spaces/1731919250519?context=%7B%22contextType%22%3A%22chat%22%7D'
